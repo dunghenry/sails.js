25967e45f229f3e14de3cfca3feea93c471464e1
dunghenry <trandungksnb6930082001>
2022-11-29 23:13:32 +0700

    initial commit

---
+++ api/models/Book.js
@@ -0,0 +1,13 @@
+/**
+ * Book.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    name: { type: "string", required: true },
+    author: { type: "string", required: true },
+  },
+};
